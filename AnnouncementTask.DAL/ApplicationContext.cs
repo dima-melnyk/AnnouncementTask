25a1d649054fccb32372dba1ffe91424e5011d80
Dmytro Melnyk <dmytro.melnyk.v@gmail.com>
2021-11-24 18:41:39 +0200

    feat: add dbcontext and announcement entity

---
+++ AnnouncementTask.DAL/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using AnnouncementTask.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnnouncementTask.DAL
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<Announcement> Announcements { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Announcement>()
+                .HasKey(a => a.Id);
+        }
+    }
+}
---
+++ AnnouncementTask.DAL/Entities/Announcement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AnnouncementTask.DAL.Entities
+{
+    class Announcement
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
